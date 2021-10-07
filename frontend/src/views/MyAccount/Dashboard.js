22c3e40840a6c12bb33131fad6142aa62094bf9c
speed-shot-77 <wdev555@gmail.com>
2021-10-07 18:07:41 -0300

    'dineli_spell'

--- frontend/src/views/MyAccount/Dashboard.js
+++ frontend/src/views/MyAccount/Dashboard.js
@@ -7,13 +7,13 @@ class Dashboard extends Component {
     return (
       <>
         <div className="d-content-highlight d-font-strong d-black d-text-30" style={{padding: '24px 4%', display: 'flex', alignItems: 'center'}}>
-          Welcome to dienli.com, Your source for Tokenized real estate
+          Welcome to dineli.com, Your source for Tokenized real estate
           <img className="img-mobile" src="imgs/account/logo.png" alt="" style={{marginLeft: 'auto'}} />
         </div>
         <div className="d-font-strong d-white d-text-24" style={{padding: '24px 12px'}}>
           Check out dineli's latest properties: dineli Marketplace
           Please use the menu links to view your portfolio and manage your account.
-          Need assistance? Please reach out to us via our chat below, or email us at: help@dienli.com.
+          Need assistance? Please reach out to us via our chat below, or email us at: help@dineli.com.
         </div>
       </>
     )
