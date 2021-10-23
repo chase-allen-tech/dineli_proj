8214d7f60d0a4e7cd30c384d48750f7e6e9c141b
speed-shot-77 <wdev555@gmail.com>
2021-10-23 11:08:27 -0300

    'delete'

--- frontend/src/services/calc.jsx
+++
@@ -1,7 +0,0 @@
-export const getFee = (amount, price) => {
-	return parseFloat(amount) * parseFloat(price) * 0.1;
-}
-
-export const getTotalWithFee = (amount, price) => {
-	return parseFloat(amount) * parseFloat(price) * 1.1;
-}
